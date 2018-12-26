8465f1eb9686bf5ca8dec7bc1d1a01615388f0b4
Rossana <rossana_matera@libero.it>
2018-12-26 20:13:52 +0100

    override

--- src/main/java/com/iota/iri/service/dto/FindTransactionsResponse.java
+++ src/main/java/com/iota/iri/service/dto/FindTransactionsResponse.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.iota.iri.service.API;
 
+/*
 /**
  * 
  * Contains information about the result of a successful {@code findTransactions} API call.
@@ -11,7 +12,24 @@ import com.iota.iri.service.API;
  * 
  */
 public class FindTransactionsResponse extends AbstractResponse {
-	
+
+
+	@Override
+	public boolean equals(Object obj4){
+		boolean equalsvalue = true;
+		if (obj4 == null) { return false; }
+		if (obj4 == this) { return true; }
+		return equalsvalue;
+	}
+
+	@Override
+	public int hashCode() {
+		int value4 = 0;
+		int result4 = 0;
+		result4 = (value4 / 11);
+		return result4;
+	}
+
   /**
     * The transaction hashes which are returned depend on your input. 
     * For each specified input value, the command will return the following:
