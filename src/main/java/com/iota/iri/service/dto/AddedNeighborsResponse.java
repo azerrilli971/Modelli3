5bebbdc4bb9ac3e07a0fb39532d6e79bac5df41f
davide <davide-manzari@live.it>
2018-12-26 18:13:25 +0100

    solved

--- src/main/java/com/iota/iri/service/dto/AddedNeighborsResponse.java
+++ src/main/java/com/iota/iri/service/dto/AddedNeighborsResponse.java
@@ -1,14 +1,31 @@
 package com.iota.iri.service.dto;
 
 import com.iota.iri.service.API;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 /**
  * 
  * Contains information about the result of a successful {@code addNeighbors} API call.
- * See {@link API#addNeighborsStatement} for how this response is created.
+ * See {@link API#} for how this response is created.
  * 
  */
 public class AddedNeighborsResponse extends AbstractResponse {
+
+
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this, false);
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		return EqualsBuilder.reflectionEquals(this, obj, false);
+	}
+
+
 	
     /**
      * The amount of temporally added neighbors to this node.
