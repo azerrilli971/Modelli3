c724f31e1dc199b9bd3d4b621bf504fa2dceef05
davide <davide-manzari@live.it>
2019-01-03 19:59:33 +0100

    solved

--- src/main/java/com/iota/iri/conf/TipSelConfig.java
+++ src/main/java/com/iota/iri/conf/TipSelConfig.java
@@ -22,12 +22,18 @@ public interface TipSelConfig extends Config {
      */
     int getBelowMaxDepthTransactionLimit();
 
-    interface Descriptions {
+    abstract static class Descriptions {
 
-        String MAX_DEPTH = "The maximal number of previous milestones from where you can perform the random walk";
-        String ALPHA = "Parameter that defines the randomness of the tip selection. " +
+        private Descriptions(){
+            super();
+        }
+
+
+
+        public static final String MAX_DEPTH = "The maximal number of previous milestones from where you can perform the random walk";
+        public static final String ALPHA = "Parameter that defines the randomness of the tip selection. " +
                 "Should be a number between 0 to infinity, where 0 is most random and infinity is most deterministic.";
-        String BELOW_MAX_DEPTH_TRANSACTION_LIMIT = "The maximal number of unconfirmed transactions that may be analyzed in " +
+        public static final String BELOW_MAX_DEPTH_TRANSACTION_LIMIT = "The maximal number of unconfirmed transactions that may be analyzed in " +
                 "order to find the latest milestone the transaction that we are stepping on during the walk approves";
     }
 }
